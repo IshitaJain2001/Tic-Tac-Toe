e27602e4cf5f6648f688fc42004e30155f0b68aa
Ishita <ishitajain385@gmail.com>
2024-06-24 12:20:26 +0530

    new changes

--- src/Square.jsx
+++ src/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Square(props) {
   return (
     <div className='border border-gray-500 flex justify-center items-center w-[100px]' onClick={props.onClick}>
-     <h3>{props.value}</h3> 
+     <h2>{props.value}</h2> 
     </div>
   )
 }
